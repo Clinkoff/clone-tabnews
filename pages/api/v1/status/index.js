979f209434b57539eaccb511fb83e684f7b348d5
Felipe Clink <Febatbr@outlook.com>
2026-02-15 19:16:05 -0400

    adicionando `jsconfig,json`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,7 @@
-import database from "../../../../infra/database";
+import database from "infra/database";
 
 async function status(request, response) {
   const result = await database.query("SELECT 1+ 1 as sum");
-  console.log(result);
   response.status(200).json({ status: "ok" });
 }
 
