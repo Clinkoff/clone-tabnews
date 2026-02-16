bf949e168f7ecf754fd1b9624f73b57b0251c4da
Felipe Clink <Febatbr@outlook.com>
2026-02-15 23:10:48 -0400

    melhoria no tratamento de erros em database.js deixando mais robusto

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,37 @@
 import database from "infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+ 1 as sum");
-  response.status(200).json({ status: "ok" });
+  const atualizadoEm = new Date().toISOString();
+  const versaoPostgresResult = await database.query("Show server_version;");
+  const versaoPostgres = versaoPostgresResult.rows[0].server_version;
+
+  const conexaoMaximasResult = await database.query("SHOW max_connections;");
+  const conexaoMaxima = conexaoMaximasResult.rows[0].max_connections;
+
+  const nomeDb = process.env.POSTGRES_DB;
+
+  const conexaoAtivaResult = await database.query({
+    text: `
+    SELECT count(*)::int AS total
+    FROM pg_stat_activity
+    WHERE datname = $1;
+  `,
+    values: [nomeDb],
+  });
+
+  const conexaoAtiva = conexaoAtivaResult.rows[0].total
+
+  console.log(conexaoAtiva)
+  response.status(200).json({
+    atualizado_em: atualizadoEm,
+    dependencies: {
+      database: {
+        versao: versaoPostgres,
+        conexao_maxima: parseInt(conexaoMaxima),
+        conexao_ativa: conexaoAtiva,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,13 @@
 test("get para /api/v1/status deveria retornar status 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  expect(responseBody.atualizado_em).toBeDefined();
+  expect(responseBody.dependencies.database.versao).toBeDefined();
+  expect(responseBody.dependencies.database.conexao_maxima).toBeDefined();
+  expect(responseBody.dependencies.database.conexao_ativa).toEqual(1);
+
+  expect(responseBody.dependencies.database.versao).toEqual("16.12")
 });
